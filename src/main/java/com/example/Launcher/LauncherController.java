82ed177fac8734e73e7c27ac9ac35d7f1acc604e
lenzgavin@gmail.com <lenzgavin@gmail.com>
2024-09-18 19:05:38 -0400

    added container for toon entries and added an "Add Toon" button

--- src/main/java/com/example/Launcher/LauncherController.java
+++ src/main/java/com/example/Launcher/LauncherController.java
@@ -1,22 +1,30 @@
 package com.example.Launcher;
 
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 
 public class LauncherController {
 
     @FXML
     private Button PlayButton;
+    private Button AddToon;
     private MenuButton Group;
     private MenuButton Options;
     private MenuButton EndSelected;
     private MenuButton UnselectedAll;
     private MenuButton EndAll;
+    private ListView ToonList;
 
 
     @FXML
     protected void playButton() {}
+
+    @FXML
+    protected void addToonButton() {}
 
     @FXML
     protected void groupButton() {}
@@ -32,4 +40,7 @@ public class LauncherController {
 
     @FXML
     protected void endAllButton() {}
+
+    @FXML
+    protected void toonListView() {}
 }
