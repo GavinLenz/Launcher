82ac4b5dcbef9a742357c2d306f17271425fca2b
lenzgavin@gmail.com <lenzgavin@gmail.com>
2024-09-18 18:00:19 -0400

    built the skeleton of the launcher, renamed example code to Launcher

--- src/main/java/com/example/Launcher/LauncherApplication.java
+++ src/main/java/com/example/Launcher/LauncherApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LauncherApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(LauncherApplication.class.getResource("Launcher.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 320);
+        stage.setTitle("Toontown Launcher");
+        stage.resizableProperty().setValue(Boolean.FALSE);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/Launcher/LauncherController.java
@@ -0,0 +1,35 @@
+package com.example.Launcher;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+
+public class LauncherController {
+
+    @FXML
+    private Button PlayButton;
+    private MenuButton Group;
+    private MenuButton Options;
+    private MenuButton EndSelected;
+    private MenuButton UnselectedAll;
+    private MenuButton EndAll;
+
+
+    @FXML
+    protected void playButton() {}
+
+    @FXML
+    protected void groupButton() {}
+
+    @FXML
+    protected void optionsButton() {}
+
+    @FXML
+    protected void endSelectedButton() {}
+
+    @FXML
+    protected void unselectAllButton() {}
+
+    @FXML
+    protected void endAllButton() {}
+}
