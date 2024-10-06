1c90c42da3750f5bce1f007b1a2029dfa38ecbdf
mbal125 <MBalkrisno665@outlook.com>
2024-10-06 19:07:22 +0100

    Launcher file added

---
+++ src/main/java/com/example/Launcher/utils/Launcher.java
@@ -0,0 +1,46 @@
+package com.example.Launcher.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder;
+import java.util.Map;
+
+
+public class Launcher {
+    private String gameserver;
+    private String cookie;
+    private String manifest;
+    private String path;
+
+    public Launcher (String gameserver, String cookie, String manifest) {
+        this.gameserver = gameserver;
+        this.cookie = cookie;
+        this.manifest = manifest;
+
+        // get path from config file instead
+        path = "C:/Program Files (x86)/Toontown Rewritten/";
+
+        launchGame();
+    }
+
+    public void launchGame() {
+        ProcessBuilder pb = new ProcessBuilder(path + "TTREngine64.exe");
+
+        // IMPORTANT - path cannot have the executable file included
+        // sets the working directory (also where start() will run)
+        pb.directory(new File(path));
+
+        // clears current environment variables and adds gameserver and cookie
+        Map<String, String> env = pb.environment();
+        env.clear();
+        env.put("TTR_GAMESERVER", gameserver);
+        env.put("TTR_PLAYCOOKIE", cookie);
+        
+        // executes the command
+        try {
+            pb.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
